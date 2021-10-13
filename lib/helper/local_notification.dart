e321a019038a9fd6777732102f30e0f92ed7cf5b
Ananda13th <pramonoananda@gmail.com>
2021-10-13 14:44:33 +0700

    Ubah Nama Channel

--- lib/helper/local_notification.dart
+++ lib/helper/local_notification.dart
@@ -3,44 +3,26 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 
 class LocalNotification {
   static Future<void> showNotification(RemoteMessage payload) async {
-    var android = AndroidInitializationSettings('app_icon');
+    var android = AndroidInitializationSettings('logo_rs');
     var initiallizationSettingsIOS = IOSInitializationSettings();
     var initialSetting = new InitializationSettings(android: android, iOS: initiallizationSettingsIOS);
     final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
     flutterLocalNotificationsPlugin.initialize(initialSetting);
 
-    // Parsing ID Notifikasi
-    final int idNotification = 1;
-
-
     // Daftar jenis notifikasi dari aplikasi.
 
-    const AndroidNotificationDetails androidPlatformChannelSpecifics = AndroidNotificationDetails(
-        'BBD', 'Notification', 'All Notification is Here',
+    const AndroidNotificationDetails androidDetails = AndroidNotificationDetails(
+        'high_importance_channel',
+        'Notification',
+        'All Notification is Here',
         importance: Importance.max,
         priority: Priority.high,
         ticker: 'ticker',
-        icon: "app_icon");
-    const NotificationDetails platformChannelSpecifics = NotificationDetails(android: androidPlatformChannelSpecifics);
-
+        icon: "logo_rs"
+    );
+    const iOSDetails = IOSNotificationDetails();
+    const NotificationDetails platformChannelSpecifics = NotificationDetails(android: androidDetails, iOS: iOSDetails);
     // Menampilkan Notifikasi
-    await flutterLocalNotificationsPlugin.show(
-        idNotification, payload.notification!.title, payload.notification!.body, platformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(0, payload.notification!.title, payload.notification!.body, platformChannelSpecifics);
   }
-
-  // Future<void> notificationHandler(GlobalKey<NavigatorState> navigatorKey) async {
-  //   // Pengaturan Notifikasi
-  //
-  //   // AndroidInitializationSettings default value is 'app_icon'
-  //   const AndroidInitializationSettings initializationSettingsAndroid = AndroidInitializationSettings('logo_bbd_sm');
-  //   final InitializationSettings initializationSettings = InitializationSettings(android: initializationSettingsAndroid);
-  //
-  //   // Handling notifikasi yang di tap oleh pengguna
-  //   flutterLocalNotificationsPlugin.initialize(initializationSettings,
-  //       onSelectNotification: (String payload) async {
-  //         if (payload != null) {
-  //           NavigatorNavigate().go(navigatorKey, payload);
-  //         }
-  //       });
-  // }
 }
