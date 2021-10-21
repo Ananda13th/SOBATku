a5fd3733833367e2a7f12ffe980033654726fa81
Ananda13th <pramonoananda@gmail.com>
2021-10-21 13:06:32 +0700

    Ubah idDokter menjadi kodeDokter

--- lib/service/jadwal_dokter_service.dart
+++ lib/service/jadwal_dokter_service.dart
@@ -6,9 +6,9 @@ import 'package:sobatku/model/jadwal_dokter.dart';
 class JadwalService {
   var baseUrl = URL.devAddress;
 
-  Future<List<JadwalDokter>> getJadwalDokter(String idDokter, String hari) async {
+  Future<List<JadwalDokter>> getJadwalDokter(String idSpesialisasi, String hari) async {
     final response = await http.get(
-      Uri.parse(baseUrl + "jadwal/$idDokter/$hari"),
+      Uri.parse(baseUrl + "jadwal/$idSpesialisasi/$hari"),
       headers: URL.createHeader(),);
     if(response.statusCode == 200) {
       final data = json.decode(response.body);
@@ -18,9 +18,9 @@ class JadwalService {
     }
   }
 
-  Future<List<JadwalDokter>> getJadwalDokterById(int idDokter) async {
+  Future<List<JadwalDokter>> getJadwalDokterById(String kodeDokter) async {
     final response = await http.get(
-        Uri.parse(baseUrl + "jadwal/$idDokter"),
+        Uri.parse(baseUrl + "jadwal/$kodeDokter"),
         headers: URL.createHeader()
     );
     if(response.statusCode == 200) {
