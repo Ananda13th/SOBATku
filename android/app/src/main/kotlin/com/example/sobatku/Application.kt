b31ebd2b5f215caae3b317b03fce2f44fa84762b
Ananda13th <pramonoananda@gmail.com>
2021-10-21 13:09:18 +0700

    AutoStart Service

--- android/app/src/main/kotlin/com/example/sobatku/Application.kt
+++ android/app/src/main/kotlin/com/example/sobatku/Application.kt
@@ -12,10 +12,20 @@
 //class Application : FlutterApplication(), PluginRegistrantCallback {
 //    override fun onCreate() {
 //        super.onCreate()
+//        createChannel();
 //        FlutterFirebaseMessagingBackgroundService.setPluginRegistrant(this);
 //    }
 //    override fun registerWith(registry: PluginRegistry) {
 //        PathProviderPlugin.registerWith(registry.registrarFor("io.flutter.plugins.pathprovider.PathProviderPlugin"));
 //        FlutterLocalNotificationsPlugin.registerWith(registry!!.registrarFor("com.dexterous.flutterlocalnotifications.FlutterLocalNotificationsPlugin"));
+//    }
+//    private fun createChannel(){
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            // Create the NotificationChannel
+//            val name = getString(R.string.default_notification_channel_id)
+//            val channel = NotificationChannel(name, "default", NotificationManager.IMPORTANCE_HIGH)
+//            val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+//            notificationManager.createNotificationChannel(channel)
+//        }
 //    }
 //}
