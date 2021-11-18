95b8e1647f363cb342a1a64244886763fdc87562
Ananda13th <pramonoananda@gmail.com>
2021-11-18 13:19:35 +0700

    Tambah Opsi Lazy Loading

--- lib/service/dokter_service.dart
+++ lib/service/dokter_service.dart
@@ -19,4 +19,25 @@ class DokterService {
       throw Exception("Failed");
     }
   }
+
+  Future<List<Dokter>> getDokterLazy(int increment, int panjangData) async {
+    final response = await http.get(
+      Uri.parse(baseUrl + "dokter/all/1"),
+      headers: URL.createHeader(),
+    );
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      List<Dokter> tempList = List<Dokter>.from(data['data'].map((dokter) => Dokter.fromJson(dokter)));
+      List<Dokter> editedList = List.empty(growable: true);
+      for (var i = panjangData; i < panjangData + increment; i++) {
+        if(i > tempList.length)
+          print("Sudah Semua");
+        else
+         editedList.add(tempList[i]);
+      }
+      return editedList;
+    } else {
+      throw Exception("Failed");
+    }
+  }
 }
