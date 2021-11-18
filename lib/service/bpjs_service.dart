2ec5c6755895736f1ffec93b85a50aa40a110e9a
Ananda13th <pramonoananda@gmail.com>
2021-11-18 13:24:32 +0700

    Service Bridging Dengan BPJS

---
+++ lib/service/bpjs_service.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+
+import 'package:sobatku/helper/url_helper.dart';
+import 'package:http/http.dart' as http;
+
+class BpjsService {
+  var baseUrl = URL.devAddress;
+
+  Future<String> cekAtivasi(String noBpjs) async {
+    final response = await http.get(
+      Uri.parse(baseUrl + "bpjs/$noBpjs"),
+      headers: URL.createHeader(),
+    );
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      if(data['error_code'] == "200" && data['data'].toString().toLowerCase() == "aktif")
+        return "aktif";
+      else
+        return data['message'].toString();
+    } else {
+      throw Exception("Failed");
+    }
+  }
+
+  Future<String> cekRujukan(String noBpjs) async {
+    final response = await http.get(
+      Uri.parse(baseUrl + "bpjs/rujukan/$noBpjs"),
+      headers: URL.createHeader(),
+    );
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      print(data);
+      if(data['error_code'] == "200")
+        return "aktif";
+      else
+        return data['message'];
+    } else {
+      throw Exception("Failed");
+    }
+  }
+}
