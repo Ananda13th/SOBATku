cabf3b409dee2cc5ee4fbef25e1b69f6ca848625
Ananda13th <pramonoananda@gmail.com>
2021-11-18 13:16:00 +0700

    Tambah Dialog Belum Login

--- lib/helper/constant.dart
+++ lib/helper/constant.dart
@@ -1,7 +1,40 @@
 
 import 'package:flutter/material.dart';
+import 'package:sobatku/page/sign_in.dart';
 
 class Constant {
   // static Color color = Color.fromARGB(255,11,128,75);
   static Color color = Color.fromARGB(255,31,141,145);
+
+  static Future<void> alertBelumLogin(BuildContext context) async {
+    await showDialog(
+      context: context,
+      builder: (context) {
+        return SimpleDialog(
+          title: Center(child: const Text('Harap Masuk Dahulu')),
+          children: <Widget>[
+            Column(
+              children: [
+                Row(
+                  children: [
+                    Flexible(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: ElevatedButton(
+                              onPressed: (){ Navigator.of(context).push(
+                                  new MaterialPageRoute(builder: (context) => new SignIn()));},
+                              style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Constant.color)),
+                              child: Center(child: const Text('MASUK', style: TextStyle(fontWeight: FontWeight.bold)))
+                          ),
+                        )
+                    )
+                  ],
+                ),
+              ],
+            )
+          ],
+        );
+      },
+    );
+  }
 }
