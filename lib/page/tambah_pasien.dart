b8dec438ea17fbc28a244f19606f0d562d2739f1
Ananda13th <pramonoananda@gmail.com>
2021-11-30 14:14:26 +0700

    Tambah Fungsi Cek BPJS saat Tambah BPJS

--- lib/page/tambah_pasien.dart
+++ lib/page/tambah_pasien.dart
@@ -3,8 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:sobatku/helper/constant.dart';
 import 'package:sobatku/helper/shared_preferences.dart';
 import 'package:sobatku/helper/toastNotification.dart';
+import 'package:sobatku/service/bpjs_service.dart';
 import 'package:sobatku/service/pasien_service.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
 import 'halaman_utama.dart';
+
 
 class TambahPasien extends StatefulWidget {
   @override
@@ -18,14 +22,17 @@ class TambahPasienState extends State<TambahPasien> {
   TextEditingController noBpjsField = TextEditingController();
   TextEditingController noKtpField = TextEditingController();
   TextEditingController namaField = TextEditingController();
+  TextEditingController noBpjsController = TextEditingController();
 
   late PasienService pasienService;
+  late BpjsService bpjsService;
   late String userId;
 
   @override
   void initState() {
     super.initState();
     pasienService = PasienService();
+    bpjsService = BpjsService();
 
     SharedPreferenceHelper.getUser().then((value) {
       setState(() {
@@ -77,40 +84,6 @@ class TambahPasienState extends State<TambahPasien> {
                             },
                           ),
                           SizedBox(height: 5),
-                          // TextFormField(
-                          //   controller: noBpjsField,
-                          //   keyboardType: TextInputType.number,
-                          //   autovalidateMode: AutovalidateMode.always,
-                          //   decoration: const InputDecoration(
-                          //     border: OutlineInputBorder(),
-                          //     icon: Icon(Icons.confirmation_number),
-                          //     labelText: 'Nomor BPJS',
-                          //   ),
-                          //   validator: (String? value) {
-                          //     if (value == null || value.isEmpty) {
-                          //       return 'Mohon Isikan Data';
-                          //     }
-                          //     return null;
-                          //   },
-                          // ),
-                          // SizedBox(height: 5),
-                          // TextFormField(
-                          //   controller: noKtpField,
-                          //   keyboardType: TextInputType.number,
-                          //   autovalidateMode: AutovalidateMode.always,
-                          //   decoration: const InputDecoration(
-                          //     border: OutlineInputBorder(),
-                          //     icon: Icon(Icons.confirmation_num_outlined),
-                          //     labelText: 'Nomor KTP',
-                          //   ),
-                          //   validator: (String? value) {
-                          //     if (value == null || value.isEmpty) {
-                          //       return 'Mohon Isikan Data';
-                          //     }
-                          //     return null;
-                          //   },
-                          // ),
-                          // SizedBox(height: 5),
                           TextFormField(
                             controller: noRmField,
                             keyboardType: TextInputType.number,
@@ -136,17 +109,78 @@ class TambahPasienState extends State<TambahPasien> {
                                     if (!_formKey.currentState!.validate()) {
                                       ToastNotification.showNotification('Harap Isi Semua Data', context, Colors.red);
                                     } else {
+                                      /** CARI PASIEN PADA DATABASE, KEMBALIAN SERVICE BERUPA NOMOR BPJS PASIEN **/
                                       pasienService.searchPasien(noRmField.text, namaField.text).then(
-                                        (value) {
-                                          if(value == false) {
+                                        (noBpjsPasien) {
+                                          if(noBpjsPasien == false) {
                                             ToastNotification.showNotification('Pasien Tidak Ditemukan', context, Colors.red);
                                           }
-                                          else if(value == true){
-                                            pasienService.createPairing(noRmField.text, userId).then((value) => ToastNotification.showNotification(value + ", Harap Cek Nomor BPJS Pasein", context, Constant.color));
-                                            Future.delayed(Duration(seconds: 1)).then((value) =>
-                                              Navigator.of(context).pushReplacement(
-                                                  new MaterialPageRoute(builder: (context) => new MyApp()))
-                                            );
+                                          else {
+                                            /** BUAT PAIRING ANTARA PASIEN DAN USER **/
+                                            setState(() {
+                                              noBpjsController.text = noBpjsPasien;
+                                            });
+                                            Alert(
+                                              context: context,
+                                              title: "Cek Nomor BPJS Pasien",
+                                              content: Column(
+                                                children: <Widget>[
+                                                  TextField(
+                                                    keyboardType: TextInputType.number,
+                                                    controller: noBpjsController..text,
+                                                    decoration: InputDecoration(
+                                                      icon: Icon(Icons.account_circle),
+                                                      labelText: "Nomor BPJS",
+                                                    ),
+                                                  ),
+                                                ],
+                                              ),
+                                              buttons: [
+                                                DialogButton(
+                                                  onPressed: () {
+                                                    pasienService.createPairing(noRmField.text, userId);
+                                                    ToastNotification.showNotification("Berhasil Menambah Pasien", context, Constant.color);
+                                                    Future.delayed(Duration(seconds: 2)).then((value) =>
+                                                        Navigator.of(context).pushReplacement(
+                                                            new MaterialPageRoute(builder: (context) => new MyApp()))
+                                                    );
+                                                  },
+                                                  child: Text(
+                                                    "Lewati dan Simpan",
+                                                    style: TextStyle(color: Colors.white, fontSize: 16, ),
+                                                    textAlign: TextAlign.center,
+                                                  ),
+                                                ),
+                                                DialogButton(
+                                                  onPressed: () {
+                                                    /** CEK STATUS KEAKTIFAN BPJS **/
+                                                    bpjsService.cekAtivasi(noBpjsController.text).then((value) {
+                                                      if (value == "aktif") {
+                                                        pasienService.createPairing(noRmField.text, userId);
+                                                        /** BILA AKTIF, NOMOR BPJS AKAN DIUPDATE **/
+                                                        pasienService.updateNoBpjs(noBpjsController.text, noRmField.text).then((value) {
+                                                          if (value) {
+                                                            ToastNotification.showNotification("Berhasil Menambah Pasien", context, Constant.color);
+                                                            Future.delayed(Duration(seconds: 2)).then((value) =>
+                                                                Navigator.of(context).pushReplacement(
+                                                                    new MaterialPageRoute(builder: (context) => new MyApp()))
+                                                            );
+                                                          }
+                                                          else
+                                                            ToastNotification.showNotification("Gagal Menambah Pasien", context, Colors.red);
+                                                        });
+                                                      }
+                                                      else
+                                                        ToastNotification.showNotification("Nomor BPJS "+ value.toString(), context, Colors.red);
+                                                    });
+                                                  },
+                                                  child: Text(
+                                                    "Simpan",
+                                                    style: TextStyle(color: Colors.white, fontSize: 20),
+                                                  ),
+                                                )
+                                              ]
+                                            ).show();
                                           }
                                         }
                                       );
