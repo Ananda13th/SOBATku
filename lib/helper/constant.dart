c8efa99cc82eb52d8efd7d0eeb3c81c8512761c9
Ananda13th <pramonoananda@gmail.com>
2021-12-15 14:03:04 +0700

    Pisah fungsi login alert

--- lib/helper/constant.dart
+++ lib/helper/constant.dart
@@ -1,40 +1,7 @@
-
 import 'package:flutter/material.dart';
-import 'package:sobatku/page/sign_in.dart';
 
+/// CLASS UNTUK SEMUA YANG TETAP SEPERTI TEMA, TEXTSTYLE DLL ///
 class Constant {
-  // static Color color = Color.fromARGB(255,11,128,75);
-  static Color color = Color.fromARGB(255,31,141,145);
-
-  static Future<void> alertBelumLogin(BuildContext context) async {
-    await showDialog(
-      context: context,
-      builder: (context) {
-        return SimpleDialog(
-          title: Center(child: const Text('Harap Masuk Dahulu')),
-          children: <Widget>[
-            Column(
-              children: [
-                Row(
-                  children: [
-                    Flexible(
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: ElevatedButton(
-                              onPressed: (){ Navigator.of(context).push(
-                                  new MaterialPageRoute(builder: (context) => new SignIn()));},
-                              style: ButtonStyle(backgroundColor: MaterialStateProperty.all<Color>(Constant.color)),
-                              child: Center(child: const Text('MASUK', style: TextStyle(fontWeight: FontWeight.bold)))
-                          ),
-                        )
-                    )
-                  ],
-                ),
-              ],
-            )
-          ],
-        );
-      },
-    );
-  }
+  static Color color = Color.fromARGB(255,0,153,0);
+  // static Color color = Color.fromARGB(255,31,141,145);
 }
