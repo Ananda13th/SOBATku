0b75471fa1c38aa7d66d7d9acec6377ea0bb93ec
Ananda13th <pramonoananda@gmail.com>
2021-12-15 13:59:23 +0700

    tambah foto

--- lib/model/jadwal_dokter.g.dart
+++ lib/model/jadwal_dokter.g.dart
@@ -10,6 +10,7 @@ JadwalDokter _$JadwalDokterFromJson(Map<String, dynamic> json) {
   return JadwalDokter(
     nama: json['nama'] as String,
     hari: json['hari'] as int,
+    foto: json['foto'] as String,
     kodeDokter: json['kode_dokter'] as String,
     jadwalPraktek: (json['jadwal'] as List<dynamic>)
         .map((e) => Jadwal.fromJson(e as Map<String, dynamic>))
@@ -22,6 +23,7 @@ Map<String, dynamic> _$JadwalDokterToJson(JadwalDokter instance) =>
     <String, dynamic>{
       'nama': instance.nama,
       'hari': instance.hari,
+      'foto': instance.foto,
       'kode_dokter': instance.kodeDokter,
       'jadwal': instance.jadwalPraktek,
       'aktif': instance.aktif,
