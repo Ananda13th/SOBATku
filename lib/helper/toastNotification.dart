98f4ec272a1bfaf876cff298a425fc4340d683f3
Ananda13th <pramonoananda@gmail.com>
2021-12-15 14:02:06 +0700

    Ubah durasi

--- lib/helper/toastNotification.dart
+++ lib/helper/toastNotification.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_styled_toast/flutter_styled_toast.dart';
 
 class ToastNotification {
-  // static Color color = Color.fromARGB(255,11,128,75);
+
   static ToastFuture showNotification (String text, BuildContext buildContext, Color color) {
     return showToast(
       text,
@@ -14,7 +14,7 @@ class ToastNotification {
       reverseAnimation: StyledToastAnimation.fade,
       position: StyledToastPosition.center,
       animDuration: Duration(seconds: 1),
-      duration: Duration(seconds: 2),
+      duration: Duration(seconds: 3),
       curve: Curves.elasticOut,
       reverseCurve: Curves.linear,
     );
