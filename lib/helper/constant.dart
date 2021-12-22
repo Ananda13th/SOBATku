a6071efbf07c07cf9b0a8dd8014bce8ce7353ea0
Ananda13th <pramonoananda@gmail.com>
2021-12-22 14:10:21 +0700

    Ubah warna dasar

--- lib/helper/constant.dart
+++ lib/helper/constant.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 
 /// CLASS UNTUK SEMUA YANG TETAP SEPERTI TEMA, TEXTSTYLE DLL ///
 class Constant {
-  static Color color = Color.fromARGB(255,0,153,0);
-  // static Color color = Color.fromARGB(255,31,141,145);
+  // static Color color = Color.fromARGB(255,0,153,0);
+  static Color color = Color.fromARGB(255,31,141,145);
 }
